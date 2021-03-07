fe51f9aeab12b5a7baed432d9eab9efbdf8b1665
JGreenOS <jsciteach@gmail.com>
2021-03-07 17:21:45 -0500

    working index.js file

--- index.js
+++ index.js
@@ -9,94 +9,165 @@ const Intern = require("./lib/intern");
 const fs = require('fs');
 const inquirer = require('inquirer');
 const path = require('path');
-//const render = require( enter the template name here)
+const util = require('util');
+const render = require("./src/templatehelpercode.js");
+//const render = require( enter the template name here when the array is built out)
 const { validate } = require("@babel/types");
 
+//arrays to hold information
+const writeFileAsync = util.promisify(fs.writeFileSync);
 const people = [];
+const ids = [];
 
-function mainfunction() {
+//output directory will need to go here
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 
-    //start with manager
-    function makeManager() {
+
+//start team
+
+//workflow - start with manager, add choice for next role of employee.  Add engineer and intern
+const startApp = () => {
+
+    console.log("Answer the questions to begin building the team. You will start with the manager first");
+    const managerData = () => {
         inquirer.prompt(
-            [  //input manager id, name, email, officenumb
+            [
                 {
-                    type: 'input',
-                    message: 'Enter the employee number',
-                    name: 'EmployeeNumber',
-                    validate: (value) => { if (value) { return true } else { return 'Required - Please input a value' } },
-                },
-                {
-                    type: 'input',
-                    message: 'Enter the first and last name of the manager',
-                    name: 'ManagerName',
-                    validate: (value) => { if (value) { return true } else { return 'Required - Please input a value' } },
-                },
-                {
-                    type: 'input',
-                    message: 'Enter the full email address of the manager',
-                    name: 'ManagerEmailAddress',
-                    validate: (value) => { if (value) { return true } else { return 'Required - Please input a value' } },
+                    type: "input",
+                    message: "Enter the employee number",
+                    name: "employeeNumber",
                 },
 
                 {
-                    type: 'number',
-                    message: 'Enter the manager\'\s office number',
-                    name: 'OfficeNumber',
-                    validate: (value) => { if (value) { return true } else { return 'Required - Please input a value' } },
-
+                    type: "input",
+                    message: "Enter the manager's first and last name",
+                    name: "managerName",
+                },
+                {
+                    type: "input",
+                    message: "Enter the manager's email address",
+                    name: "managerEmail",
+                },
+                {
+                    type: "number",
+                    message: "Enter the manager's office number",
+                    name: "managerOffice",
                 },
 
-            ]).then(answers => {
-                const manager = new Manager(answers.EmployeeNumber, answers.ManagerName, answers.ManagerEmailAddress, answers.OfficeNumber);
-                people.push(manager);
+
+
+            ])
+            .then(answers => {
+                const manager = new Manager(answers.employeeNumber, answers.managerName, answers.managerEmail, answers.managerOffice)
+                people.push(manager)
+                ids.push(answers.employeeNumber);
+                addTeamMember();
             });
     }
-    //makeManager has to be inside of the main function for inquirer to run
-    makeManager();
-}
+    const addTeamMember = () => {
+        inquirer.prompt(
+            [
+                {
+                    type: "list",
+                    name: "role",
+                    message: "Choose a role. If data entry is complete, select Create Team Roster",
+                    choices: [
+                        "Engineer",
+                        "Intern",
+                        "Data Entry Complete, Create Team Roster please",
+                    ]
+                }
+            ]
+        )
+            .then(choice => {
+                switch (choice.role) {
+                    case "Engineer":
+                        engineerData();
+                        break;
+                    case "Intern":
+                        internData();
+                        break;
+                    default: makeRoster();
+                }
+            });
 
-
-//create inquirer questions
-
-//ask which role to input (engineer, intern or done)
-//     {
-//         type:'list',
-//         message: 'Engineer or Intern?',
-//         name: 'Role',
-//         choices: ["Engineer", "Intern", "All Set - Build Team"],
-//         when: function(UserInput) {
-//             return 
-
-//     },
-// },
-
-
-
-
-
-
-//if intern, goto intern questions
-
-//if engineer, go to engineer questions
-
-//is team done?
-function makeHTML() {
-    // Create the output directory if the output path doesn't exist
-    if (!fs.existsSync(OUTPUT_DIR)) {
-      fs.mkdirSync(OUTPUT_DIR)
     }
-    fs.writeFileSync(outputPath, render(people), "utf-8");
-  }
-makeHTML();
-mainfunction();
-//make HTML
+    const engineerData = () => {
+        inquirer.prompt(
+            [
+                {
+                    type: "input",
+                    name: "engineerId",
+                    message: "Enter the Engineer's employee id"
+                },
+                {
+                    type: "input",
+                    name: "engineerName",
+                    message: "Enter the Engineer's name"
+                },
+                {
+                    type: "input",
+                    name: "engineerEmail",
+                    message: "Enter the Engineer's Email Address"
+                },
+                {
+                    type: "input",
+                    name: "engineerGitHub",
+                    message: "Enter the Engineer's GitHub username"
+                },
+            ])
+            .then(answers => {
+                const engineer = new Engineer(answers.engineerId, answers.engineerName, answers.engineerEmail, answers.engineerGithub)
+                people.push(engineer)
+                ids.push(answers.engineerId);
+                addTeamMember();
+            });
+    }
+    const internData = () => {
+        inquirer.prompt(
+            [
+                {
+                    type: "input",
+                    message: "Enter the employee number",
+                    name: "employeeNumber",
+                },
 
-userInput()
- .then((answers) => writeFileAsync('index.html', mainfunction(answers)))
- .then(() => console.log('Successfully wrote index.html file'))
-.catch((err) => console.error(err));
+                {
+                    type: "input",
+                    message: "Enter the intern's first and last name",
+                    name: "internName",
+                },
+                {
+                    type: "input",
+                    message: "Enter the intern's email address",
+                    name: "internEmail",
+                },
+                {
+                    type: "input",
+                    message: "Enter the intern's current school",
+                    name: "internSchool",
+                },
 
-module.exports = mainfunction;
 
+
+            ])
+            .then(answers => {
+                const intern = new Intern(answers.employeeNumber, answers.internName, answers.internEmail, answers.internSchool)
+                people.push(intern)
+                ids.push(answers.employeeNumber);
+                addTeamMember();
+            });
+    }
+    function makeRoster() {
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileAsync(outputPath, render(people), "utf-8");
+    };
+
+    managerData();
+
+}
+startApp();
