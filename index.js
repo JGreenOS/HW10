0d289390a694d2cde2779168d95e60030e4b3fef
JGreenOS <jsciteach@gmail.com>
2021-03-06 22:49:11 -0500

    index js with pseudocode

---
+++ index.js
@@ -0,0 +1,85 @@
+const { EmptyError } = require("rxjs")
+
+//pull in all classes
+const Employee = require("./lib/employee");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+//pull in utilties
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+//const render = require( enter the template name here)
+const { validate } = require("@babel/types");
+
+const people = [];
+
+function overallMakeFunctionWork () {
+
+
+    //start with manager
+    function makeManager() {
+        inquirer.prompt(
+            [  //input manager id, name, email, officenumb
+                {
+                    type: 'input',
+                    message: 'Enter the employee number',
+                    name: 'EmployeeNumber',
+                    validate: (value) => { if (value) { return true } else { return 'Required - Please input a value' } },
+                },
+                {
+                    type: 'input',
+                    message: 'Enter the first and last name of the manager',
+                    name: 'ManagerName',
+                    validate: (value) => { if (value) { return true } else { return 'Required - Please input a value' } },
+                },
+                {
+                    type: 'input',
+                    message: 'Enter the full email address of the manager',
+                    name: 'ManagerEmailAddress',
+                    validate: (value) => { if (value) { return true } else { return 'Required - Please input a value' } },
+                },
+
+                {
+                    type: 'number',
+                    message: 'Enter the manager\'\s office number',
+                    name: 'OfficeNumber',
+                    validate: (value) => { if (value) { return true } else { return 'Required - Please input a value' } },
+
+                },
+
+            ]).then(answers => {
+                const manager = new Manager(answers.EmployeeNumber, answers.ManagerName, answers.ManagerEmailAddress, answers.OfficeNumber);
+            });
+        }
+    }
+
+//create inquirer questions
+
+//ask which role to input (engineer, intern or done)
+    //     {
+    //         type:'list',
+    //         message: 'Engineer or Intern?',
+    //         name: 'Role',
+    //         choices: ["Engineer", "Intern", "All Set - Build Team"],
+    //         when: function(UserInput) {
+    //             return 
+
+    //     },
+    // },
+
+
+
+
+
+
+//if intern, goto intern questions
+
+//if engineer, go to engineer questions
+
+//is team done?
+
+//create team
+
+//make HTML
+
